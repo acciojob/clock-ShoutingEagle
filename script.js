4db74de1024d995ee57af4129202a4fade8184e7
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-08-01 03:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const ptag = document.createElement('p');
+
+var clock = setInterval(clockTiming ,1000);
+
+function clockTiming (){
+    let dateTime =new Date();
+    let convert = dateTime.toLocaleString();
+    ptag.id = 'timer';
+    ptag.innerHTML = convert;
+    document.body.appendChild(ptag);
+}
